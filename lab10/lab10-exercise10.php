5b9f4b1c565fc4adfec69684adb9b44d161643ed
zzy3333 <19307130172@fudan.edu.cn>
2020-06-07 21:25:28 +0800

    lab10

--- lab10/lab10-exercise10.php
+++ lab10/lab10-exercise10.php
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-<?php 
+<?php
 
-require_once('config.php'); 
+require_once('config.php');
 
 /*
  Displays a list of genres
@@ -40,7 +39,7 @@ function outputSingleGenre($row) {
     echo '</div>'; // end class=card
 }
 
-/* 
+/*
   Constructs a link given the genre id and a label (which could
   be a name or even an image tag
 */
@@ -54,84 +53,24 @@ function constructGenreLink($id, $label) {
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chapter 14</title>
-    <link href="semantic/semantic.css" rel="stylesheet"> 
-  </head>
+    <link href="semantic/semantic.css" rel="stylesheet">
+</head>
 <body>
 <main class="ui container">
-      <div class="ui secondary segment">
-         <h1>List of Links</h1>
-      </div>
-      
-      <div class="ui segment">  
-         <div class="ui six doubling cards">
-            <?php outputGenres(); ?>  
-         </div>
-      </div>            
+    <div class="ui secondary segment">
+        <h1>List of Links</h1>
+    </div>
+
+    <div class="ui segment">
+        <div class="ui six doubling cards">
+            <?php outputGenres(); ?>
+        </div>
+    </div>
 </main>
 
 </body>
-=======
-<?php 
-
-require_once('config.php'); 
-
-/*
- Displays a list of genres
-*/
-function outputGenres() {
-   try {
-      $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-               
-
-      $pdo = null;
-   }
-   catch (PDOException $e) {
-      die( $e->getMessage() );
-   }
-}
-
-/*
- Displays a single genre
-*/
-function outputSingleGenre($row) {
-
-}
-
-/* 
-  Constructs a link given the genre id and a label (which could
-  be a name or even an image tag
-*/
-function constructGenreLink($id, $label) {
-
-}
-
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chapter 14</title>
-    <link href="semantic/semantic.css" rel="stylesheet"> 
-  </head>
-<body>
-<main class="ui container">
-      <div class="ui secondary segment">
-         <h1>List of Links</h1>
-      </div>
-      
-      <div class="ui segment">  
-         <div class="ui six doubling cards">
-            <?php outputGenres(); ?>  
-         </div>
-      </div>            
-</main>
-
-</body>
->>>>>>> 76bafeb7f92081de6194569b17cfce1440e92ef6
 </html>
--- lab10/lab10-exercise8.php
+++ lab10/lab10-exercise8.php
@@ -1,28 +1,27 @@
-<<<<<<< HEAD
-<?php 
+<?php
 
-require_once('config.php'); 
+require_once('config.php');
 
 /*
  Displays the list of artist links on the left-side of page
 */
 function outputArtists() {
-   try {
-         $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
-         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-         $sql = "select * from Artists order by LastName limit 0,30";
-         $result = $pdo->query($sql);
-         while ($row = $result->fetch()) {
+    try {
+        $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "select * from Artists order by LastName limit 0,30";
+        $result = $pdo->query($sql);
+        while ($row = $result->fetch()) {
             echo '<a href="' . $_SERVER["SCRIPT_NAME"] . '?id=' . $row['ArtistID'] . '" class="';
             if (isset($_GET['id']) && $_GET['id'] == $row['ArtistID']) echo 'active ';
             echo 'item">';
             echo $row['LastName'] . '</a>';
-         }
-         $pdo = null;
-   }
-   catch (PDOException $e) {
-      die( $e->getMessage() );
-   }
+        }
+        $pdo = null;
+    }
+    catch (PDOException $e) {
+        die( $e->getMessage() );
+    }
 }
 
 /*
@@ -68,10 +67,10 @@ function outputSinglePainting($row) {
 <!DOCTYPE html>
 <html>
 <head>
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Chapter 14</title>
-   <link href="semantic/semantic.css" rel="stylesheet"> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chapter 14</title>
+    <link href="semantic/semantic.css" rel="stylesheet">
 </head>
 <body>
 <main class="ui container">
@@ -95,66 +94,4 @@ function outputSinglePainting($row) {
 </main>
 
 </body>
-=======
-<?php 
-
-require_once('config.php'); 
-
-/*
- Displays the list of artist links on the left-side of page
-*/
-function outputArtists() {
-   try {
-         $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
-         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-         $sql = "select * from Artists order by LastName limit 0,30";
-         $result = $pdo->query($sql);
-         while ($row = $result->fetch()) {
-            echo '<a href="' . $_SERVER["SCRIPT_NAME"] . '?id=' . $row['ArtistID'] . '" class="';
-            if (isset($_GET['id']) && $_GET['id'] == $row['ArtistID']) echo 'active ';
-            echo 'item">';
-            echo $row['LastName'] . '</a>';
-         }
-         $pdo = null;
-   }
-   catch (PDOException $e) {
-      die( $e->getMessage() );
-   }
-}
-
-/*
- Displays the list of paintings for the artist id specified in the id query string
-*/
-function outputPaintings() {
-
-}
-
-/*
- Displays a single painting
-*/
-function outputSinglePainting($row) {
-
-}
-
-?>
-<!DOCTYPE html>
-<html>
-<head>
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Chapter 14</title>
-   <link href="semantic/semantic.css" rel="stylesheet"> 
-</head>
-<body>
-   <main class="ui container">
-      <div class="ui secondary segment">
-         <h1>User Input</h1>
-      </div>
-      <div class="ui segment">
-
-      </div>
-   </main>
-
-</body>
->>>>>>> 76bafeb7f92081de6194569b17cfce1440e92ef6
 </html>
