2010cf60f843c8fd4fd6fcb21d230df90219c0f7
zzy3333 <19307130172@fudan.edu.cn>
2020-06-08 16:03:12 +0800

    lab11

--- lab11/lab11-exercise1.php
+++ lab11/lab11-exercise1.php
@@ -1,11 +1,11 @@
 <html lang="en">
 <head>
 
-<!-- Latest compiled and minified Bootstrap Core CSS -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <!-- Latest compiled and minified Bootstrap Core CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
-   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-   <title>Exercise 13-1 | Using Cookies</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Exercise 11-1 | Using Cookies</title>
 </head>
 
 <body>
@@ -15,7 +15,7 @@
 
 <?php
 function getLoginForm(){
-   return "
+    return "
 <form action='' method='post' role='form'>
 <div class ='form-group'>
   <label for='username'>Username</label>
@@ -26,26 +26,67 @@ function getLoginForm(){
   <input type='password' name='pword' class='form-control'/>
 </div>
 <input type='submit' value='Login' class='form-control' />
-
 </form>";
 }
-?>
- <div class="container theme-showcase" role="main">  
-      <div class="jumbotron">
-        <h1>
-<?php
-   require_once("config.php");
-   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     echo "Login attempted";
-   }
-   else{
-     echo "No Post detected";
-   } 
-?>
+function validLogin(){
+    $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+    //very simple (and insecure) check of valid credentials.
+    $sql = "SELECT * FROM Credentials WHERE Username=:user and Password=:pass";
 
-</h1>
-      </div>
-<?php echo getLoginForm(); ?>
- </div>
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(':user',$_POST['username']);
+    $statement->bindValue(':pass',$_POST['pword']);
+    $statement->execute();
+    if($statement->rowCount()>0){
+        return true;
+    }
+    return false;
+}
+?>
+<div class="container theme-showcase" role="main">
+    <div class="jumbotron">
+        <h1>
+            <?php
+            require_once("config.php");
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                echo "Login attempted";
+            }
+            else{
+                echo "No Post detected";
+            }
+            ?>
+        </h1>
+    </div>
+    <?php
+    $loggedIn=false;
+    if(!$loggedIn){
+        if (!isset($_COOKIE['Username'])){
+            echo getLoginForm();
+        }
+        else{
+            echo "This is some content";
+        }
+    } else{
+        echo "This is some content";
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(validLogin()){
+            // add 1 day to the current time for expiry time
+            $expiryTime = time()+60*60*24;
+            setcookie("Username", $_POST['username'], $expiryTime);
+        }
+        else{
+            echo "login unsuccessful";
+        }
+    }
+    if(isset($_COOKIE['Username'])){
+        echo "Welcome ".$_COOKIE['Username'];
+    }
+    else{
+        echo "No Post detected";
+    }
+    ?>
+</div>
 </body>
 </html>
